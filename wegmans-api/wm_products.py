fad5dc4fd237a7e4eecf306a41c968e7cd4bd614
Joshua Yoder <git@jpfyoder.com>
2020-02-08 13:21:45 -0500

    implemented basic wegmans products api wrapper

--- wegmans-api/wm_products.py
+++ wegmans-api/wm_products.py
@@ -1,5 +1,8 @@
 """
-Wegman Products API Wrapper
+Wegmans Products API Wrapper
+Written at Brickhack 2020
+author: Joshua Yoder <jpfyoder>
+author: Dhaval Shrishrimal
 """
 
 #external packages
@@ -9,12 +12,49 @@ import json
 #import authorization key from auth.py, hidden from git
 from auth import API_KEY
 
-#authorization header
+#authorization header and url macro
 header = '?api-version=2018-10-18&Subscription-Key=' + API_KEY
 url = 'https://api.wegmans.io/products/'
 
-print(API_KEY)
+def get_location(sku, store):
+    response = requests.get(url + str(sku) + '/locations/' + str(store) + header)
+    return json.loads(response.json())
 
-response = requests.get('https://api.wegmans.io/products/categories?api-version=2018-10-18&Subscription-Key=' + API_KEY)
+def get_locations(sku):
+    response = requests.get(url + str(sku) + '/locations' + header)
+    return json.loads(response.json())
 
-print(response.json())
+def get_categories():
+    response = requests.get(url + 'categories' + header)
+    return json.loads(response.json())
+
+def get_category(category):
+    response = requests.get(url + 'categories/' + str(category) + header)
+    return json.loads(response.json())
+
+def get_product(sku):
+    response = requests.get(url + str(sku) + header)
+    return json.loads(response.json())
+
+def get_products():
+    response = requests.get(url + header)
+    return json.loads(response.json())
+
+#def search_products():
+#    return
+
+def get_price(sku, store):
+    response = requests.get(url + str(sku) + '/prices/' + str(store) + header)
+    return json.loads(response.json())
+
+#def get_prices(sku, stores):
+#    return
+
+def get_availabilities(sku):
+    response = requests.get(url + str(sku) + '/availabilities' + header)
+    return json.loads(response.json())
+
+def get_availability(sku, store):
+    response = requests.get(url + str(sku) + '/availabilities/' + str(store))
+    return json.loads(response.json())
+
