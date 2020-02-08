417d594328a07e497788ce21bb0f0fb525f8475a
Ezequiel Salas <es3621@rit.edu>
2020-02-08 13:50:17 -0500

    changed to return

--- diet/DietMain.py
+++ diet/DietMain.py
@@ -1,8 +1,11 @@
+import ShoppingList
+import FoodItem
+from WeeklyValues import SEX
 
-
-def main(args):
+def main():
 	shopping_cart = ShoppingList(SEX.Male)
-
-	
-
+	fake_food = foodObject("fake_food",69,{"zinc":12,"iron":2,"calcium":4})
+	print(fake_food.name)
 	print("Hello World")
+
+main()
