de9cd6ae70725705580791802a8d3da463f9b3a9
Ezequiel Salas <es3621@rit.edu>
2020-02-08 12:34:41 -0500

    "Created FoodItem"

---
+++ diet/FoodItem.py
@@ -0,0 +1,15 @@
+
+class foodObject():
+    def __init__(self,name,cost,nutrition_facts,location):
+        self.name = name
+        self.cost = cost
+        self.nutrition_facts = nutrition_facts
+        self.location = location
+
+    def get_nut_specific(self,thing):
+        if thing in self.nutrition_facts:
+            return self.nutrition_facts[thing]
+        else:
+            return "Does not contain "+ thing
+
+
