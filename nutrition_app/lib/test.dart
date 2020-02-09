b6b44253482606bd01ec0ee9017de90c2759b96f
jmb3471 <47160881+jmb3471@users.noreply.github.com>
2020-02-09 02:39:02 -0500

    test

---
+++ nutrition_app/lib/test.dart
@@ -0,0 +1,457 @@
+KNOWLEDGE TRANSFER
+ ANDROID
+ TENSORFLOW
+ FLUTTER
+ FIREBASE
+ CONTACT
+HTTP post request in Flutter.
+
+JAN 06 2019- POSTED BY BRIJESH Comments Off
+Spread the love
+In this tutorial, we are going to build Flutter APP to send data to JSONPlaceholder(Fake Online REST API for Testing and Prototyping). This is an example app that can post a text data to a web server and receive the same data as a response if resource successfully created. Having the ability to do HTTP Post Request Flutter app to the remote server is required for most apps.
+
+Flutter Post Request
+1. Add Package
+In this example, we are using an HTTP dart package for creating an HTTP post request. It is a composable, Future-based library for making HTTP requests. This package contains high-level functions and classes that make it easy to consume HTTP resources.
+
+Add following dependencies to your package’s pubspec.yaml file.You can find the latest version of the http package on the pub website.
+
+dependencies:
+  http: ^0.12.0
+1
+2
+dependencies:
+  http: ^0.12.0
+2.Create Post Class
+First, we’ll need to create a Post class that contains the data from our network request. It will also include a factory constructor that allows us to create a Post from json.
+
+
+class Post {
+  final String userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({this.userId, this.id, this.title, this.body});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+
+  Map toMap() {
+    var map = new Map<String, dynamic>();
+    map["userId"] = userId;
+    map["title"] = title;
+    map["body"] = body;
+
+    return map;
+  }
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+class Post {
+  final String userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({this.userId, this.id, this.title, this.body});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+
+  Map toMap() {
+    var map = new Map<String, dynamic>();
+    map["userId"] = userId;
+    map["title"] = title;
+    map["body"] = body;
+
+    return map;
+  }
+}
+3. Make a HTTP POST request
+In this example, we’ll send a sample post to the JSONPlaceholder REST API using the http.post()method.
+
+Future<Post> createPost(String url, {Map body}) async {
+  return http.post(url, body: body).then((http.Response response) {
+    final int statusCode = response.statusCode;
+
+    if (statusCode < 200 || statusCode > 400 || json == null) {
+      throw new Exception("Error while fetching data");
+    }
+    return Post.fromJson(json.decode(response.body));
+  });
+}
+
+// Call Web services
+new RaisedButton(
+                  onPressed: () async {
+                    Post newPost = new Post(
+                        userId: "123", id: 0, title: titleControler.text, body: bodyControler.text);
+                    Post p = await createPost(CREATE_POST_URL,
+                        body: newPost.toMap());
+                    print(p.title);
+                  },
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+Future<Post> createPost(String url, {Map body}) async {
+  return http.post(url, body: body).then((http.Response response) {
+    final int statusCode = response.statusCode;
+
+    if (statusCode < 200 || statusCode > 400 || json == null) {
+      throw new Exception("Error while fetching data");
+    }
+    return Post.fromJson(json.decode(response.body));
+  });
+}
+
+// Call Web services
+new RaisedButton(
+                  onPressed: () async {
+                    Post newPost = new Post(
+                        userId: "123", id: 0, title: titleControler.text, body: bodyControler.text);
+                    Post p = await createPost(CREATE_POST_URL,
+                        body: newPost.toMap());
+                    print(p.title);
+                  },
+The http.post() method returns a Future that contains a Response.The http.Response class contains the same date received from a successful http call.
+
+Complete example
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Post {
+  final String userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({this.userId, this.id, this.title, this.body});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+
+  Map toMap() {
+    var map = new Map<String, dynamic>();
+    map["userId"] = userId;
+    map["title"] = title;
+    map["body"] = body;
+
+    return map;
+  }
+}
+
+Future<Post> createPost(String url, {Map body}) async {
+  return http.post(url, body: body).then((http.Response response) {
+    final int statusCode = response.statusCode;
+
+    if (statusCode < 200 || statusCode > 400 || json == null) {
+      throw new Exception("Error while fetching data");
+    }
+    return Post.fromJson(json.decode(response.body));
+  });
+}
+
+class MyApp extends StatelessWidget {
+  final Future<Post> post;
+
+  MyApp({Key key, this.post}) : super(key: key);
+  static final CREATE_POST_URL = 'https://jsonplaceholder.typicode.com/posts';
+  TextEditingController titleControler = new TextEditingController();
+  TextEditingController bodyControler = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: "WEB SERVICE",
+      theme: ThemeData(
+        primaryColor: Colors.deepOrange,
+      ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Create Post'),
+          ),
+          body: new Container(
+            margin: const EdgeInsets.only(left: 8.0, right: 8.0),
+            child: new Column(
+              children: <Widget>[
+                new TextField(
+                  controller: titleControler,
+                  decoration: InputDecoration(
+                      hintText: "title....", labelText: 'Post Title'),
+                ),
+                new TextField(
+                  controller: bodyControler,
+                  decoration: InputDecoration(
+                      hintText: "body....", labelText: 'Post Body'),
+                ),
+                new RaisedButton(
+                  onPressed: () async {
+                    Post newPost = new Post(
+                        userId: "123", id: 0, title: titleControler.text, body: bodyControler.text);
+                    Post p = await createPost(CREATE_POST_URL,
+                        body: newPost.toMap());
+                    print(p.title);
+                  },
+                  child: const Text("Create"),
+                )
+              ],
+            ),
+          )),
+    );
+  }
+}
+
+void main() => runApp(MyApp());
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90
+91
+92
+93
+94
+95
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Post {
+  final String userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({this.userId, this.id, this.title, this.body});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+
+  Map toMap() {
+    var map = new Map<String, dynamic>();
+    map["userId"] = userId;
+    map["title"] = title;
+    map["body"] = body;
+
+    return map;
+  }
+}
+
+Future<Post> createPost(String url, {Map body}) async {
+  return http.post(url, body: body).then((http.Response response) {
+    final int statusCode = response.statusCode;
+
+    if (statusCode < 200 || statusCode > 400 || json == null) {
+      throw new Exception("Error while fetching data");
+    }
+    return Post.fromJson(json.decode(response.body));
+  });
+}
+
+class MyApp extends StatelessWidget {
+  final Future<Post> post;
+
+  MyApp({Key key, this.post}) : super(key: key);
+  static final CREATE_POST_URL = 'https://jsonplaceholder.typicode.com/posts';
+  TextEditingController titleControler = new TextEditingController();
+  TextEditingController bodyControler = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: "WEB SERVICE",
+      theme: ThemeData(
+        primaryColor: Colors.deepOrange,
+      ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Create Post'),
+          ),
+          body: new Container(
+            margin: const EdgeInsets.only(left: 8.0, right: 8.0),
+            child: new Column(
+              children: <Widget>[
+                new TextField(
+                  controller: titleControler,
+                  decoration: InputDecoration(
+                      hintText: "title....", labelText: 'Post Title'),
+                ),
+                new TextField(
+                  controller: bodyControler,
+                  decoration: InputDecoration(
+                      hintText: "body....", labelText: 'Post Body'),
+                ),
+                new RaisedButton(
+                  onPressed: () async {
+                    Post newPost = new Post(
+                        userId: "123", id: 0, title: titleControler.text, body: bodyControler.text);
+                    Post p = await createPost(CREATE_POST_URL,
+                        body: newPost.toMap());
+                    print(p.title);
+                  },
+                  child: const Text("Create"),
+                )
+              ],
+            ),
+          )),
+    );
+  }
+}
+
+void main() => runApp(MyApp());
