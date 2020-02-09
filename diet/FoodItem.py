5213ca8ae49fd8dda850c5c0efb5cd3387884647
Ezequiel Salas <es3621@rit.edu>
2020-02-08 19:53:31 -0500

    comments

--- diet/FoodItem.py
+++ diet/FoodItem.py
@@ -1,3 +1,10 @@
+"""
+File:FoodItem.py
+@authors: Jonathan Baxley, Ezequiel Salas
+Class object for a food item. Generic data structure
+to keep track of information for any food item.
+"""
+
 
 class foodObject:
     def __init__(self,name,cost,nutrition_facts):
@@ -8,18 +15,28 @@ class foodObject:
         self.nutrition_facts = nutrition_facts
 
     def get_nut_specific(self, thing):
+        """
+        Returns the nutrition value for the
+        specified type of nutrition, in miligrams
+        :param thing: String name of nutrition
+        :return: float amount of nutrition in miligrams
+        """
         if (self.nutrition_facts == []):
             return 0
+        #iterate through the nutrition dictionary
         for x in self.nutrition_facts:
+            #if the current key equals the one we are looking for
             if x['type'] == thing:
                 quantity = x['quantity']
-
+                #if there exists something in the quantity
                 if quantity != '' and quantity != 'None':
                     try:
                         quantity_split = quantity.split(" ")
                         float_quant = float(quantity_split[0])
                         if (len(quantity_split) > 1):
+                            #check to see if the value is in grams
                             if (quantity_split[1] == 'g'):
+                                #convert to miligrams
                                 float_quant = float_quant * 1000
                         print(quantity_split[0])
                         return float_quant
@@ -32,5 +49,8 @@ class foodObject:
 
 
     def toString(self):
+        """
+        :return: name,cost
+        """
         return self.name + ","+str(self.cost)
 
